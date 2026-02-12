b5a6968ce86d3ddabea65ba42ee2d634e4ba04a3
Alyssa Mobley <alyssamoble2004@gmail.com>
2026-02-12 17:32:47 -0500

    Added all text for login page

--- Frontend/src/pages/LoginPage/LoginPage.jsx
+++ Frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import Login from "./LoginPage/LoginPage.jsx";
-
+import "./LoginPage.css";
 
 function Login() {
     const [username, setUsername] = useState("");
@@ -13,18 +12,24 @@ function Login() {
     };
 
     return (
-        <div style={{ maxWidth: "400px", margin: "50px auto" }}>
-            {/* creating a heading for the login form */}
-            <h2>Login</h2>
+        <div style={{margin: "50px auto" }}>
+            <h1>Wayne State University</h1>
+            <h2>Academica Login</h2>
+
+            <p>You have reached a page that requires authentication,
+                please enter your Wayne State AccessID and password.</p>
 
             <form onSubmit={handleSubmit}>
+                <p>Your Wayne State AccessID</p>
                 <input
                     type="text"
                     placeholder="AccessID"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                 />
+                <br/>
 
+                <p>Your Password</p>
                 <input
                     type="password"
                     placeholder="Password"
@@ -32,8 +37,18 @@ function Login() {
                     onChange={(e) => setPassword(e.target.value)}
                 />
 
+                <br/>
+
                 <button type="submit">Login</button>
             </form>
+
+            <p>Forgot your AccessID or password?</p>
+
+            <p>Don't have an AccessID or password?</p>
+
+            <p>Need help with other WSU technologies?</p>
+
+            <p>By using this service you agree to adhere to WSU computing policies and guidelines.</p>
         </div>
     );
 }
