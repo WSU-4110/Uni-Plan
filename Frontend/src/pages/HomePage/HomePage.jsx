d1ee678eb7b114011cf2e36706858025dabff092
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2026-02-18 21:02:46 -0500

    Fix JSX syntax for Course Registration header

--- Frontend/src/pages/HomePage/HomePage.jsx
+++ Frontend/src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import CourseSearch from "../../components/CourseSearch/CourseSearch";
 function HomePage() {
   return (
     <div>
-      <h1 style={{ padding: "20px 20px 0" }}Course Registration</h1>
+      <h1 style={{ padding: "20px 20px 0" }}>Course Registration</h1>
       <CourseSearch />
     </div>
   );
