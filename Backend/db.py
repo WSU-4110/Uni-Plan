0da5d95f7f3ea1cbe2ddeaec4a62f8bcee31fcbc
Nahyun Kim <nahyunkim036@gmail.com>
2026-03-07 23:43:33 -0500

    WIP: update db.py

--- Backend/db.py
+++ Backend/db.py
@@ -4,9 +4,9 @@ import psycopg2.extras
 def get_conn():
     conn = psycopg2.connect(
         host="localhost",
-        dbname="your_db_name",
-        user="your_username",
-        password="your_password",
+        dbname="UniPlan-test",
+        user="kimnahyun",
+        password="5178",
         cursor_factory=psycopg2.extras.RealDictCursor
     )
     return conn
