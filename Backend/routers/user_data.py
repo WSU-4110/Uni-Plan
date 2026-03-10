476a4b5889f3d7a976c1091d6fb6e69b1e78ca8a
ZacharyLukas <ho8532@wayne.edu>
2026-03-10 16:27:25 -0400

    begun work on save route

--- Backend/routers/user_data.py
+++ Backend/routers/user_data.py
@@ -1,4 +1,19 @@
 from fastapi import APIRouter, Query
 from db import get_conn
+from main import CourseList
 
 router = APIRouter()
+
+
+@router.post("/save")
+def save_courses(courses: CourseList):
+
+    #query for injection
+
+
+    for course_id in CourseList.course_ids:
+
+        #append query with new course
+        return
+    
+    return {"received": courses.course_ids}
