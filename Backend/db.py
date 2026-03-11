985138a847a2d04043e891947f77cfdd609ce717
Nahyun Kim <nahyunkim036@gmail.com>
2026-03-10 22:54:15 -0400

    Move database credentials to environment variables

--- Backend/db.py
+++ Backend/db.py
@@ -1,12 +1,17 @@
+import os
 import psycopg2
 import psycopg2.extras
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_conn():
     conn = psycopg2.connect(
-        host="localhost",
-        dbname="UniPlan-test",
-        user="username",  # change to your PostgreSQL username
-        password="1234", # change to your PostgreSQL password
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
         cursor_factory=psycopg2.extras.RealDictCursor
     )
     return conn
