93c5b1f652924912e3f340709cc64c967a2637af
Nahyun Kim <nahyunkim036@gmail.com>
2026-03-10 21:35:57 -0400

    Connect plans router to plan save service

--- Backend/routers/plans.py
+++ Backend/routers/plans.py
@@ -1,27 +1,14 @@
 from fastapi import APIRouter, Query
-from db import get_conn
 from schemas.plan_schema import CourseList
+from services.plan_service import save_courses_to_plan
 
 router = APIRouter()
 
-
 @router.post("/save")
 def save_courses(courses: CourseList):
-
-    conn = get_conn()
-    try:
-        cur = conn.cursor()
-
-        # delete previous plan with same user and name
-        delete_sql = "DELETE FROM plan WHERE student_id = %s AND name = %s"
-        cur.execute(delete_sql, (courses.user, courses.name))
-
-        for course_id in courses.course_ids:
-            sql = "INSERT INTO plan(student_id, id, term_id, name) Values (%s, %s, %s, %s)"
-            cur.execute(sql,(courses.user, course_id, courses.term, courses.name))
-
-        conn.commit()
-    finally:
-        conn.close()
-
-    return {"received": courses.course_ids}
+    return save_courses_to_plan(
+            course_ids=courses.course_ids,
+            user=courses.user,
+            term=courses.term,
+            name=courses.name
+        )
