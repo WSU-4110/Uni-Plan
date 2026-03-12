e28a38e2f52e4fd4178f8fdd0ccf21fde1e48211
Zachary Lukas <ho8532@wayne.edu>
2026-03-11 20:31:43 -0400

    added search by course_id

--- Backend/routers/courses.py
+++ Backend/routers/courses.py
@@ -53,6 +53,7 @@ def search_courses(
     SELECT
         s."CRN" AS crn,
         s.term_id AS term_id,
+        c.id as course_id,
         c.subject AS subject,
         c.course_number AS course_number,
         c.title AS title,
@@ -77,7 +78,8 @@ def search_courses(
             LOWER(c.title) LIKE %(q_like)s OR
             LOWER(c.subject) LIKE %(q_like)s OR
             LOWER(c.course_number) LIKE %(q_like)s OR
-            CAST(s."CRN" AS TEXT) LIKE %(q_like)s OR
+            CAST(c.id AS TEXT) LIKE %(q_like)s OR
+            CAST(s."CRN" AS TEXT) LIKE %(q_like)s OR1
             LOWER(COALESCE(c.instructor, '')) LIKE %(q_like)s
         )
     ORDER BY c.subject, c.course_number, s."CRN"
@@ -102,7 +104,8 @@ def search_courses(
     results = []
     for r in rows:
         results.append(
-            {
+            {   
+                "courseId": r["course_id"],     #not displayed to users
                 "subject": r["subject"],
                 "courseNumber": r["course_number"],
                 "courseCode": f"{r['subject']} {r['course_number']}",
