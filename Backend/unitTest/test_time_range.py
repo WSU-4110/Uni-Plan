5797b735a45ad5e35ff7a411f9452448c91422dd
Alyssa Mobley <alyssamoble2004@gmail.com>
2026-03-25 14:47:58 -0400

    Finished all unit tests

--- Backend/unitTest/test_time_range.py
+++ Backend/unitTest/test_time_range.py
@@ -4,3 +4,13 @@ def test_format_time_range_normal():
     r={"start_min": 540, "end_min": 650}
     result = format_time_range(r)
     assert result == "09:00 - 10:50"
+
+def test_format_time_range_start_none():
+    r={"start_min": None, "end_min": 650}
+    result = format_time_range(r)
+    assert result == "TBA"
+
+def test_format_time_range_end_none():
+    r={"start_min": 540, "end_min": None}
+    result = format_time_range(r)
+    assert result == "TBA"
