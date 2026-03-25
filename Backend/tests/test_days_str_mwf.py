fe042e64589bb945b67ed5925da9d1eac0c2211a
Nahyun Kim <nahyunkim036@gmail.com>
2026-03-25 16:49:47 -0400

    tested test_days_str_mwf.py

---
+++ Backend/tests/test_days_str_mwf.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from routers.courses import days_str
+
+
+def test_days_str_mwf():
+    r = {
+        "monday": True,
+        "tuesday": False,
+        "wednesday": True,
+        "thursday": False,
+        "friday": True,
+    }
+    assert days_str(r) == "MWF"
