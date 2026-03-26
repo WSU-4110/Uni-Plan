50f3cd222a0dc746510f1b2e6d7772422043ddb3
Nahyun Kim <nahyunkim036@gmail.com>
2026-03-26 12:00:59 -0400

    tested wo calls with the same input produce different hashes

--- Backend/tests/test_minutes_to_time_str_0.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from routers.courses import minutes_to_time_str
-
-def test_minutes_to_time_str_0():
-    assert minutes_to_time_str(0) == "00:00"
