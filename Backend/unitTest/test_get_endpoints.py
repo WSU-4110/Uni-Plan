4f7f29c3ce3cb85037c80ed4a881dbb39214f60b
ZacharyLukas <ho8532@wayne.edu>
2026-03-26 14:34:00 -0400

    semi working version of tests

---
+++ Backend/unitTest/test_get_endpoints.py
@@ -0,0 +1,47 @@
+import pytest
+from fastapi.testclient import TestClient
+from Backend.main import app
+client = TestClient(app)
+
+
+
+def test_api_success():
+    payload = {"username":"habib", "password":"1234"}
+
+    response = client.post("/api/auth/login", json=payload)
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "success": True,
+        "message": "Login successful"
+    }
+
+
+def test_login_failure():
+    payload = {"username": "testuser", "password": "wrongpassword"}
+    
+    response = client.post("/login", json=payload)
+    
+    assert response.status_code == 200
+    assert response.json()["success"] is False
+    assert "Invalid" in response.json()["message"]
+
+def test_logout():
+
+    response = client.post("/api/auth/logout")
+
+    assert response.status_code == 200
+    assert response.json() == {
+            "success": True,
+            "message": "Logged out successfully"
+        }
+
+
+def test_get_response():
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "FastAPI backend is running"
+    }
